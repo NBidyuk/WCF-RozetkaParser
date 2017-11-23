143c1ccd87400aa245efc25b823be5d1362470b4
NBidyuk <nbidyuk@gmail.com>
2017-11-23 16:52:23 +0200

    1st commit

---
+++ Rozetka/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿using System.Windows.Forms;
+
+namespace Rozetka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Model = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Description = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Price = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Link = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Model,
+            this.Description,
+            this.Price,
+            this.Link});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(863, 465);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseMove);
+            this.listView1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseUp);
+            // 
+            // Model
+            // 
+            this.Model.Text = "Model";
+            this.Model.Width = 250;
+            // 
+            // Description
+            // 
+            this.Description.Text = "Description";
+            this.Description.Width = 300;
+            // 
+            // Price
+            // 
+            this.Price.Text = "Price";
+            this.Price.Width = 100;
+            // 
+            // Link
+            // 
+            this.Link.Text = "Link";
+            this.Link.Width = 200;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(694, 498);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(881, 533);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "www.rozetka.com.ua";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Model;
+        private System.Windows.Forms.ColumnHeader Description;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader Price;
+        private ColumnHeader Link;
+    }
+}
+
---
+++ Rozetka/Form1.cs
@@ -0,0 +1,161 @@
+﻿/*Реализовать приложение, которое выводит информацию о ноутбуках с сайта Rozetka.com.ua.
+Должна быть выведена минимум такая информация по каждому ноутбуку: название, цена, тип процессора, размер ОЗУ.
+В приложении должен использоваться асинхронные методы async await.*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Xml;
+using System.Net.Http;
+using System.Reflection;
+
+namespace Rozetka
+{
+    public partial class Form1 : Form
+    {
+        private List<NoteBook> productList;
+        public Form1()
+        {
+            InitializeComponent();
+            productList = new List<NoteBook>();
+            Type type = listView1.GetType();
+            PropertyInfo propertyInfo = type.GetProperty("DoubleBuffered", BindingFlags.NonPublic | BindingFlags.Instance);
+            propertyInfo.SetValue(listView1, true, null);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+             {
+
+                 
+                 WebClient client = new WebClient();
+                 client.Encoding = UTF8Encoding.UTF8;
+                 HtmlAgilityPack.HtmlDocument resultDoc = new HtmlAgilityPack.HtmlDocument();
+
+                 int pageCount = 1;
+                
+
+
+                 for (;pageCount<=35;pageCount++)
+                 {
+                     string path = String.Format("http://rozetka.com.ua/notebooks/c80004/filter/page={0}/", pageCount);
+                     string urlData = client.DownloadString(path);
+
+
+
+
+                     resultDoc.LoadHtml(urlData);
+                     var allDivlist = resultDoc.DocumentNode.Descendants().Where
+                 (x => (x.Name == "div" && x.Attributes["name"] != null && x.Attributes["name"].Value.Equals("goods_list"))).ToList();
+
+
+
+                     var AllProductsNodes = allDivlist[0].Descendants().Where(x => (x.Name == "div" && x.Attributes["class"] != null && x.Attributes["class"].Value.Contains("g-i-tile-i-box"))).ToList();
+
+                     foreach (var obj in AllProductsNodes)
+                     {
+
+                         var listName = obj.Descendants().Where(x => (x.Name == "div" && x.Attributes["class"] != null && x.Attributes["class"].Value.Contains("g-i-tile-i-title"))).ToList()[0].Descendants().Where(x => (x.Name == "a")).ToList();
+                         var name = listName[0].InnerText;
+                         var link = listName[0].GetAttributeValue("href", null);
+
+                         var listDesc = obj.Descendants().Where(x => (x.Attributes["class"] != null && x.Attributes["class"].Value.Contains("g-i-tile-short-detail"))).ToList();
+                         var description = listDesc[0].Descendants().Where(x => (x.Name == "li")).ToList()[0].InnerText;
+
+                         
+
+                         var listPrice = obj.Descendants().Where(x => (x.Attributes["class"] != null && x.Attributes["class"].Value.Contains("g-price-uah"))).ToList();
+                         var price = "";
+                         if (listPrice.Count>0)
+                         {
+                             var lprice = listPrice[0].Descendants().Where(x => (x.Name == "div")).ToList();
+                             price = listPrice[0].InnerText;
+                         }
+                         else
+                         {
+                             price = "no info";
+                         }
+                         
+
+
+
+                         NoteBook newLaptop = new NoteBook()
+                         {
+                             Name = name,
+                             Description = description,
+                             Price = price + " UAH",
+                             Link = link
+
+                         };
+
+                         productList.Add(newLaptop);
+
+                         ListViewItem item = new ListViewItem(newLaptop.Name);
+                         item.SubItems.Add(newLaptop.Description);
+                         item.SubItems.Add(newLaptop.Price);
+
+                         item.SubItems.Add(newLaptop.Link);
+                         listView1.HotTracking = true;
+
+                         listView1.Items.Add(item);
+
+                     }
+
+
+               
+                 }
+             });
+        }
+
+
+
+
+        private void listView1_MouseMove(object sender, MouseEventArgs e)
+        {
+            var hit = listView1.HitTest(e.Location);
+            if (hit.SubItem != null && hit.SubItem == hit.Item.SubItems[3])
+                listView1.Cursor = Cursors.Hand;
+            else listView1.Cursor = Cursors.Default;
+        }
+
+
+        private void listView1_MouseUp(object sender, MouseEventArgs e)
+        {
+            var hit = listView1.HitTest(e.Location);
+            if (hit.SubItem != null && hit.SubItem == hit.Item.SubItems[3])
+            {
+                var url = new Uri(hit.SubItem.Text);
+                System.Diagnostics.Process.Start(url.ToString());
+            }
+        }
+    }
+
+
+
+    public class NoteBook
+
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string Link { get; set; }
+
+    }
+
+}
+
